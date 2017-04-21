9165c09f34ced7fa900bb6924e4b16f6fd0bd211
lemo <linpeng_flexi@163.com>
2017-04-21 10:11:40 +0800

    添加sql

---
+++ src/main/resources/db/V1.0.0_init.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS T_USER;
+CREATE TABLE `t_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(40) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `age` int(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8;
